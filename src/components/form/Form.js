71ab28a1f2de99f269139e7c840775b5153d3c24
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-11-21 13:02:52 -0500

    Initial form demo

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Form.css";
 
 const Form = () => {
+    const [pageNum, setpageNum] = useState(0);
+
+    const handlePageDecrement = () => {
+        setpageNum((prev) => prev - 1);
+    };
+
+    const handlePageIncrement = () => {
+        setpageNum((prev) => prev + 1);
+    };
+
     return (
         <div className="form">
             <h1>Form</h1>
+            
+            <button onClick={handlePageDecrement}>Previous</button>
+
+            <span>{pageNum}</span>
+            <button onClick={handlePageIncrement}>Next</button>
         </div>
     );
 };
