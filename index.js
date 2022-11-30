7ea9ad9ecf691a8d5412591b597ffaaf8b2dc921
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-11-30 17:25:50 -0600

    Remove unused import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Packages
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Styles
 import "./index.css";
