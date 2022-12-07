4976d4019c02f27133992276d65cbf666a6f8f2d
tristayqc <qinchen.yao@vanderbilt.edu>
2022-12-07 00:52:27 -0600

    modified state

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
 import "./Form.css";
-import Page1 from "./Page1"
+import Page1 from "./Page1";
 
 const NUM_PAGES = 5;
 
 const Form = () => {
     const [pageNum, setpageNum] = useState(0);
-
     const handlePageDecrement = () => {
         setpageNum((prev) => prev - 1);
     };
@@ -16,12 +15,15 @@ const Form = () => {
         setpageNum((prev) => prev + 1);
     };
 
+    const [languages, setLanguages] = useState();
+    const [nationalities, setNationalites] = useState();
+    const [education, setEducation] = useState();
+
     return (
         <div className="form-container">
             <h1>Form</h1>
             <h4>Page Number: {pageNum}</h4>
             <div>PAGE CONTENT</div>
-            <Page1/>
             <div className="button-container">
                 { pageNum > 0 && 
                     <button onClick={handlePageDecrement}>Previous</button>
@@ -29,6 +31,17 @@ const Form = () => {
                 { pageNum < NUM_PAGES - 1 &&
                     <button onClick={handlePageIncrement}>Next</button>
                 }
+            </div>
+
+            <div>
+                {(() => {
+                    if (pageNum === 1) {
+                        return <Page1
+                        formLanguages={languages} setFormLanguages={setLanguages}
+                        formNationalities={nationalities} setFormNationalities={setNationalites}
+                        formEducation={education} setFormEducation={setEducation}
+                        />
+                    }})()}
             </div>
         </div>
     );
--- src/components/form/Page1.js
+++ src/components/form/Page1.js
@@ -1,34 +1,33 @@
-import React, {useState} from "react";
+import React from "react";
 import CreatableSelect from 'react-select/creatable';
 import Select from "react-select";
 
 import "./Page1.css";
 
-const Page1 = () => {
-   
-    const [selectedLanguages, setSelectedLanguages] = useState();
+const Page1 = ({formLanguages, setFormLanguages, 
+    formNationalities, setFormNationalities,
+    formEducation, setFormEducation}) => {
+
     const handleLanguageSelect = (data) =>{
-        setSelectedLanguages(data);
+        setFormLanguages(data);
     };
 
-    const [selectedNationalities, setSelectedNationalities] = useState(); 
     const handleNationalitySelect = (data) =>{
-        setSelectedNationalities(data); 
+        setFormNationalities(data); 
     }
 
-    const [selectedEducation, setSelectedEducation] = useState(); 
     const handleEducationSelect = (data) =>{
-        setSelectedEducation(data); 
+        setFormEducation(data); 
     }
     
     return (
         <div className="page1-container">
-            <h1>Basic Info</h1>
+            <h3>Basic Info</h3>
             <div className="wrapper">
                 <label>Fluent Languages
                     <CreatableSelect 
                     options={languageOpts}
-                    value={selectedLanguages}
+                    value={formLanguages}
                     onChange={handleLanguageSelect}
                     // defaultValue={[languageOptions[0], languageOptions[1]]}
                     isMulti
@@ -42,7 +41,7 @@ const Page1 = () => {
                     Nationality
                     <CreatableSelect 
                     options={nationalityOpts}
-                    value={selectedNationalities}
+                    value={formNationalities}
                     onChange={handleNationalitySelect}
                     isMulti
                     name="nationality"
@@ -55,7 +54,7 @@ const Page1 = () => {
                     Education Level
                     <Select 
                     options={eduOpts}
-                    value={selectedEducation}
+                    value={formEducation}
                     onChange={handleEducationSelect}
                     name="education"
                     className="creatable-multi-select"
