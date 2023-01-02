054c6422fe0c72cfbebd150942d768ed91090cbd
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-01-01 19:26:05 -0700

    Change file ext and format

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
     },
     {
         path: "/beneficiaries",
-        element: <Beneficiaries/>,
+        element: <Beneficiaries />,
     },
     {
         path: "/programs",
