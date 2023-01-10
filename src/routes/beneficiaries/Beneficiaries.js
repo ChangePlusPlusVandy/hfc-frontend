85e1b8f9ca82a49ae4aade73a342c52329610804
Raj Chopra <raj.chopra@vanderbilt.edu>
2023-01-09 21:10:21 -0600

    fix toggle to show alphabetical ordering

--- src/routes/beneficiaries/Beneficiaries.js
+++ src/routes/beneficiaries/Beneficiaries.js
@@ -23,6 +23,18 @@ const Beneficiaries = () => {
     const toggleArchived = () => {
         if (archivedBeneficiaries.length > 0) {
             let data = [...beneficiary,...archivedBeneficiaries];
+            data.sort((a, b) => {
+                let nameA = a.firstName.toUpperCase(); // ignore upper and lowercase
+                let nameB = b.firstName.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                  return -1;
+                }
+                if (nameA > nameB) {
+                  return 1;
+                }
+                // names must be equal
+                return 0;
+              }); 
             setBeneficiary(data);
             setArchivedBeneficiaries([]);
         } else {
