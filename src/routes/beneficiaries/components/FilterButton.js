7febe95d4a3cce6b68c7d3ca398c76df092b0ad3
tristayqc <qinchen.yao@vanderbilt.edu>
2023-01-24 21:16:32 -0600

    minor change in filterbutton

--- src/routes/beneficiaries/components/FilterButton.js
+++ src/routes/beneficiaries/components/FilterButton.js
@@ -3,7 +3,6 @@ import React from "react";
 function FilterButton(props) {
     return (
         <button
-            type="button"
             className="btn toggle-btn"
             aria-pressed={props.isPressed}
             onClick={() => props.setFilter(props.name)}
