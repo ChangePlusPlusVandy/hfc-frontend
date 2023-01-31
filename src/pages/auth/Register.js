218a8cdbb83855465b0caca7cb157134bd5e18e2
chopraj <chopraj@users.noreply.github.com>
2023-01-31 00:05:05 +0000

    Apply Prettier formatting

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,37 +1,47 @@
-import React,{useState} from 'react'
-import './Register.css'
-import {auth} from '../../../firebase/firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import React, { useState } from "react";
+import "./Register.css";
+import { auth } from "../../../firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Register = () => {
-
     const handleSubmit = async (e) => {
-      e.preventDefault();
-      console.log(email,password)
-      try{
-        const userCrediential = await createUserWithEmailAndPassword(auth,email,password);
-        console.log(userCrediential.user);
-      } catch(err) {
-        console.log(err)
-        console.log(err.message)
-      }
-      
-      
-    }
+        e.preventDefault();
+        console.log(email, password);
+        try {
+            const userCrediential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            console.log(userCrediential.user);
+        } catch (err) {
+            console.log(err);
+            console.log(err.message);
+        }
+    };
 
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    return (
+        <div className="form_container">
+            <form className="form" onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    type="text"
+                    placeholder="Email"
+                />
+                <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
 
-  return (
-    <div className='form_container'>
-      <form className='form' onSubmit={(e) => handleSubmit(e)}>
-        <input onChange={(e) => setEmail(e.target.value)} value={email} type='text' placeholder='Email'/>
-        <input onChange={(e) => setPassword(e.target.value)} value={password} type='password' placeholder='Password'/>
-        <input type='submit' value='Register'/>
-      </form>
-    </div>
-  )
-}
-
-export default Register
+export default Register;
--- webpack.config.js
+++ webpack.config.js
@@ -106,5 +106,4 @@ module.exports = {
             systemvars: true,
         }),
     ],
-
 };
