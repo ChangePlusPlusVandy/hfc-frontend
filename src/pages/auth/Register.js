f48b7c64896da2d6af61350c585093b6f1642343
Raj Chopra <raj.chopra@vanderbilt.edu>
2023-01-31 20:31:19 -0600

    add registered user to mongo

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -5,19 +5,28 @@ import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 const Register = () => {
 
+  const addUserToFb = async (uid,level=0) => {
+    const response = await fetch('http://localhost:3000/users', {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+              firebaseUID: uid,
+          }),
+      });
+    return response
+  }
+
     const handleSubmit = async (e) => {
       e.preventDefault();
       console.log(email,password)
       try{
         const userCrediential = await createUserWithEmailAndPassword(auth,email,password);
-        console.log(userCrediential.user);
-      } catch(err) {
-        console.log(err)
-        console.log(err.message)
-      }
-      
-      
-    }
+        const res = await addUserToFb(userCrediential.user.uid);
+      console.log(res)
+    } catch (err) {
+      console.log(err);
+      console.log(err.message);
+    }}
 
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
@@ -25,6 +34,7 @@ const Register = () => {
 
   return (
     <div className='form_container'>
+      <h1>Register</h1>
       <form className='form' onSubmit={(e) => handleSubmit(e)}>
         <input onChange={(e) => setEmail(e.target.value)} value={email} type='text' placeholder='Email'/>
         <input onChange={(e) => setPassword(e.target.value)} value={password} type='password' placeholder='Password'/>
