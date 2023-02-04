747031fe60fb03d9d3b20f7109b304531977f7f5
Raj Chopra <raj.chopra@vanderbilt.edu>
2023-02-04 17:47:14 -0600

    add navigate to dashboard after login or register

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import "./Login.css";
-
 import { auth } from "../../../firebase/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import {useNavigate} from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -17,6 +17,7 @@ const Login = () => {
                 password
             );
             console.log(userCreds.user);
+            navigate('/dashboard');
         } catch (err) {
             console.log(err);
             console.log(err.message);
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import "./Register.css";
 import { auth } from "../../../firebase/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import {useNavigate} from 'react-router-dom';
 
-const Register = () => {
+const Register = () => { 
+  const navigate = useNavigate();
 
   const addUserToMongo = async (uid,level=0) => {
     const response = await fetch('http://localhost:3000/users', {
@@ -23,7 +25,8 @@ const Register = () => {
       try{
         const userCrediential = await createUserWithEmailAndPassword(auth,email,password);
         const res = await addUserToMongo(userCrediential.user.uid);
-      console.log(res)
+        console.log(res)
+        navigate('/dashboard')
     } catch (err) {
       console.log(err);
       console.log(err.message);
