2fb87ad70490410daf5383019c7568ab9b843874
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-03-04 01:02:25 -0600

    Refactor links

--- src/components/NavSidebar.js
+++ src/components/NavSidebar.js
@@ -6,8 +6,8 @@ import "./NavSidebar.css";
 import HFCLogo from "../assets/images/hfc-logo-peach.png";
 import DefaultUserProfilePic from "../assets/images/default-user.png";
 import UserListIcon from "../assets/icons/user-list-icon.png";
+import IdentificiationCardIcon from "../assets/icons/identification-card-icon.png";
 import ChartLineIcon from "../assets/icons/chart-line-icon.png";
-import UserCirclePlusIcon from "../assets/icons/user-circle-plus-icon.png";
 import TabsIcon from "../assets/icons/tabs-icon.png";
 import BookIcon from "../assets/icons/book-icon.png";
 import ClipboardTextIcon from "../assets/icons/clipboard-text-icon.png";
@@ -46,10 +46,18 @@ const NavSidebar = () => {
                 <NavLink to="beneficiaries">
                     <img
                         src={UserListIcon}
-                        alt="Beneficiaries List"
+                        alt="Beneficiary Directory"
                         className="icon"
                     />
-                    <span>Beneficiaries List</span>
+                    <span>Beneficiary Directory</span>
+                </NavLink>
+                <NavLink to="users">
+                    <img
+                        src={IdentificiationCardIcon}
+                        alt="Staff Directory"
+                        className="icon"
+                    />
+                    <span>Staff Directory</span>
                 </NavLink>
                 <NavLink to="data">
                     <img
@@ -59,37 +67,21 @@ const NavSidebar = () => {
                     />
                     <span>Data Dashboard</span>
                 </NavLink>
-                <NavLink to="beneficiaries/register">
-                    <img
-                        src={UserCirclePlusIcon}
-                        alt="Register a Beneficiary"
-                        className="icon"
-                    />
-                    <span>Register a Beneficiary</span>
-                </NavLink>
                 <NavLink to="programs">
-                    <img
-                        src={TabsIcon}
-                        alt="Manage Programs"
-                        className="icon"
-                    />
-                    <span>Manage Programs</span>
+                    <img src={TabsIcon} alt="Programs" className="icon" />
+                    <span>Programs</span>
                 </NavLink>
                 <NavLink to="workshops">
-                    <img
-                        src={BookIcon}
-                        alt="Manage Workshops"
-                        className="icon"
-                    />
-                    <span>Manage Workshops</span>
+                    <img src={BookIcon} alt="Workshops" className="icon" />
+                    <span>Workshops</span>
                 </NavLink>
                 <NavLink to="assessments">
                     <img
                         src={ClipboardTextIcon}
-                        alt="Manage Assessments"
+                        alt="Assessments"
                         className="icon"
                     />
-                    <span>Manage Assessments</span>
+                    <span>Assessments</span>
                 </NavLink>
             </nav>
             <button className="logout-button" onClick={handleLogout}>
