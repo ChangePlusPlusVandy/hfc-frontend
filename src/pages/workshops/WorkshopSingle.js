004334649228986496b4ba2222eb5e1d571a0201
Zack Silver <zacksilver@Zacks-MacBook-Air.local>
2023-04-02 16:54:56 -0500

    more ethan changes

--- src/pages/workshops/WorkshopSingle.js
+++ src/pages/workshops/WorkshopSingle.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, Link } from "react-router-dom";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import Select from "react-select";
-import { Navigate } from "react-router-dom";
 
 import "react-tabs/style/react-tabs.css";
 import "./SingleWorkshop.css";
@@ -18,16 +16,21 @@ export const WorkshopSingle = () => {
     const [updateDate, setUpdateDate] = useState(new Date());
     const [editMode, setEditMode] = useState(false);
     const [hostOptions, setHostOptions] = useState([]);
+    const navigate = useNavigate();
     const deleteWorkshop = () => {
-        const requestOptions = {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                workshopID,
-            }),
-        };
-        fetch("http://localhost:3000/workshops", requestOptions);
-        Navigate(`../`);
+        try {
+            const requestOptions = {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    workshopID,
+                }),
+            };
+            fetch("http://localhost:3000/workshops", requestOptions);
+            navigate("../");
+        } catch (err) {
+            console.log(err);
+        }
     };
     const updateWorkshop = () => {
         console.log("editing");
@@ -63,43 +66,53 @@ export const WorkshopSingle = () => {
     };
     useEffect(() => {
         console.log("here");
-        fetch("http://localhost:3000/workshops?_id=" + workshopID)
-            .then((response) => response.json())
-            .then((data) => {
-                data = data[0];
-                fetch("http://localhost:3000/users/users")
-                    .then((response2) => response2.json())
-                    .then((data2) => {
-                        let tempOptions = data2
-                            .filter((item) => item.firstName && item.lastName)
-                            .map((item) => {
-                                return {
-                                    value: item._id,
-                                    label: item.firstName + " " + item.lastName,
-                                };
-                            });
-                        if (updateWorkHosts) {
-                            setUpdateWorkHosts([]);
-                        } //because useEffect gets called twice
-                        let tempHosts = data.hosts.map((item) => {
-                            for (let k = 0; k < tempOptions.length; k++) {
-                                if (item == tempOptions[k].value) {
-                                    setUpdateWorkHosts((oldArray) => [
-                                        ...oldArray,
-                                        tempOptions[k],
-                                    ]);
-                                    return tempOptions[k].label;
+        try {
+            //TODO: better error handling
+            fetch("http://localhost:3000/workshops?_id=" + workshopID)
+                .then((response) => response.json())
+                .then((data) => {
+                    data = data[0];
+                    fetch("http://localhost:3000/users/users")
+                        .then((response2) => response2.json())
+                        .then((data2) => {
+                            let tempOptions = data2
+                                .filter(
+                                    (item) => item.firstName && item.lastName
+                                )
+                                .map((item) => {
+                                    return {
+                                        value: item._id,
+                                        label:
+                                            item.firstName +
+                                            " " +
+                                            item.lastName,
+                                    };
+                                });
+                            if (updateWorkHosts) {
+                                setUpdateWorkHosts([]);
+                            } //because useEffect gets called twice
+                            let tempHosts = data.hosts.map((item) => {
+                                for (let k = 0; k < tempOptions.length; k++) {
+                                    if (item == tempOptions[k].value) {
+                                        setUpdateWorkHosts((oldArray) => [
+                                            ...oldArray,
+                                            tempOptions[k],
+                                        ]);
+                                        return tempOptions[k].label;
+                                    }
                                 }
+                            });
+                            console.log(tempHosts);
+                            if (tempHosts) {
+                                data.hosts = tempHosts;
                             }
+                            setHostOptions(tempOptions);
+                            setWorkshop(data);
                         });
-                        console.log(tempHosts);
-                        if (tempHosts) {
-                            data.hosts = tempHosts;
-                        }
-                        setHostOptions(tempOptions);
-                        setWorkshop(data);
-                    });
-            });
+                });
+        } catch (err) {
+            console.log(err);
+        }
     }, [editMode]);
     return (
         <div className="single-workshop-container">
@@ -201,17 +214,16 @@ export const WorkshopSingle = () => {
                             >
                                 Cancel
                             </button>
-                            <Link
+                            <button
                                 style={{
                                     textDecoration: "none",
                                     float: "right",
                                 }}
                                 onClick={deleteWorkshop}
                                 className="submit-button"
-                                to="../"
                             >
                                 Delete
-                            </Link>
+                            </button>
                             &emsp;&emsp;
                         </div>
                     </div>
@@ -264,6 +276,7 @@ export const WorkshopSingle = () => {
                         </div>
                         <div className="workshop-info">
                             <h3>Date</h3>
+                            {/*use divs instead of h7*/}
                             <h7>
                                 {workshop.date
                                     ? new Date(workshop.date)
@@ -324,174 +337,3 @@ export const WorkshopSingle = () => {
         </div>
     );
 };
-
-/*return (
-        <div className="single-workshop-container">
-            {editMode === true ? (
-                <div>
-                    <div>
-                        <label>
-                            <h3>
-                                Title:
-                                <input
-                                    type="text"
-                                    value={updateWorkTitle}
-                                    //name="title"
-                                    onChange={(e) =>
-                                        setUpdateWorkTitle(e.target.value)
-                                    }
-                                />
-                            </h3>
-                        </label>
-                    </div>
-                    <div>
-                        <h3>
-                            <label>
-                                Description:
-                                <input
-                                    type="text"
-                                    value={updateWorkDesc}
-                                    name="description"
-                                    onChange={(e) =>
-                                        setUpdateWorkDesc(e.target.value)
-                                    }
-                                />
-                            </label>
-                        </h3>
-                    </div>
-                    <div>
-                        <h3>Workshop Date</h3>
-                        <input
-                            type="date"
-                            id="Date"
-                            onChange={(e) => {
-                                setUpdateDate(e.target.value);
-                            }}
-                            value={updateDate}
-                        />
-                    </div>
-                    <div>
-                        <h3>
-                            <label>
-                                Hosts:
-                                <Select
-                                    options={hostOptions}
-                                    type="text"
-                                    value={updateWorkHosts}
-                                    name="description"
-                                    onChange={(e) => setUpdateWorkHosts(e)}
-                                    isMulti
-                                />
-                            </label>
-                        </h3>
-                        <div
-                            className="sortIndicator"
-                            onChange={(e) =>
-                                setUpdateWorkStatus(e.target.value)
-                            }
-                            value={updateWorkStatus}
-                        >
-                            <input type="radio" value={false} name="sortVal" />
-                            Active
-                            <input type="radio" value={true} name="sortVal" />
-                            Archived
-                        </div>
-                    </div>
-                    <button onClick={updateWorkshop} className="submit-button">
-                        Submit
-                    </button>
-                </div>
-            ) : (
-                <div className="single-workshop-container">
-                <Link to="/dashboard/workshops">&lt; back to workshop list</Link>
-                    <div className="single-workshop">
-                    <div className="single-workshop-heading">
-                        <h1>{workshop.title}</h1>
-                            <h3>Description: {workshop.description}</h3>
-                            <h3>
-                                {" "}
-                                {workshop.hosts && workshop.hosts.length > 0 ? (
-                                    <>Hosts: {workshop.hosts.join(", ")}</>
-                                ) : (
-                                    <></>
-                                )}
-                            </h3>
-                            <h3>Date: {workshop.date}</h3>
-                            <h3>
-                                Status:{" "}
-                                {workshop.archived ? (
-                                    <>archived</>
-                                ) : (
-                                    <>active</>
-                                )}
-                            </h3>
-                            <h3>
-                                {workshop.numAttendees ? (
-                                    <>
-                                        Attendees:{" "}
-                                        {workshop.numAttendees}
-                                        <br></br>
-                                        Registered Attendees:{" "}
-                                        {workshop.numRegistered}
-                                        <br></br>
-                                        Unregistered Attendees:{" "}
-                                        {workshop.numAttendees -
-                                            workshop.numRegistered}
-                                        <br></br>
-                                        Rating: {workshop.rating.toFixed(2)}
-                                    </>
-                                ) : (
-                                    <></>
-                                )}
-                            </h3>
-                        </div>
-
-                        <button
-                            onClick={enterUpdateMode}
-                            className="submit-button"
-                        >
-                            {" "}
-                            Edit Workshop
-                        </button>
-                        <button
-                            onClick={deleteWorkshop}
-                            className="submit-button"
-                        >
-                            {" "}
-                            Delete Workshop
-                        </button>
-                        <Link
-                            className="button"
-                            to="../attendance"
-                            state={{
-                                //id: workshopID,
-                            }}
-                        >
-                            Take Attendance
-                        </Link>
-                        </div>
-                        </div>
-            )}
-        </div>
-    );
-};
-
-
-            <div className="heading-buttons">
-                        <button
-                            onClick={() => editOverviewOrEnroll(false)}
-                            id="overview-button"
-                            className="tab"
-                        >
-                            Overview
-                        </button>
-                        <button
-                            onClick={() => editOverviewOrEnroll(true)}
-                            id="enrollment-button"
-                            className="tab"
-                        >
-                            Attendance
-                        </button>
-                    </div>
-
-*/
